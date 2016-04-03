6eb21777f0249b81836ef1d7fcdd9a16ba0ca0a7
Sam Lombardo <samlombardo1981@gmail.com>
2016-04-02 22:45:01 -0400

    - add test db context

---
+++ TestDbContext.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using AweSamNet.Common;
+
+namespace AweSamNet.Data.EntityFramework
+{
+    //https://msdn.microsoft.com/en-us/data/dn314431.aspx
+    public interface IDbContext
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        DbSet Set(Type entityType);
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        IEnumerable<DbEntityValidationResult> GetValidationErrors();
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        DbEntityEntry Entry(object entity);
+        Database Database { get; }
+        DbChangeTracker ChangeTracker { get; }
+        DbContextConfiguration Configuration { get; }
+    }
+
+    public class TestDbContext : DbContext, IDbContext
+    {
+        public int SaveChangesCount { get; private set; }
+        public new int SaveChanges()
+        {
+            if (ChangeTracker.HasChanges())
+            {
+                GetValidationErrors();
+
+                foreach (var entity in ChangeTracker.Entries())
+                {
+                    switch (entity.State)
+                    {
+                        case EntityState.Added:
+                            Set(entity.Entity.GetType()).Add(entity);
+                            break;
+                        case EntityState.Deleted:
+                            Set(entity.Entity.GetType()).Remove(entity);
+                            break;
+                        case EntityState.Modified:
+                        case EntityState.Detached:
+                        case EntityState.Unchanged:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            this.SaveChangesCount++;
+            return 1;
+        }
+    } 
+
+    public class TestDbSet<TEntity> : DbSet<TEntity>, IQueryable, IEnumerable<TEntity>, IDbAsyncEnumerable<TEntity>
+        where TEntity : class
+    {
+        ObservableCollection<TEntity> _data;
+        IQueryable _query;
+
+        public TestDbSet(IList<TEntity> collection = null)
+        {
+            _data = collection == null ? new ObservableCollection<TEntity>() : new ObservableCollection<TEntity>(collection);
+            _query = _data.AsQueryable();
+        }
+
+        public override TEntity Add(TEntity item)
+        {
+            if (!TryAdd(item))
+            {
+                throw new Exception(string.Format("This {0} already exists", typeof(TEntity).Name));
+            }
+            return item;
+        }
+
+        private bool TryAdd(TEntity item)
+        {
+            if (_data.Contains(item)) return false;
+
+            _data.Add(item);
+
+            return true;
+        }
+
+        public override TEntity Remove(TEntity item)
+        {
+            _data.Remove(item);
+            return item;
+        }
+
+        public override TEntity Attach(TEntity item)
+        {
+            TryAdd(item);
+            return item;
+        }
+
+        public override TEntity Create()
+        {
+            return Activator.CreateInstance<TEntity>();
+        }
+
+        public override TDerivedEntity Create<TDerivedEntity>()
+        {
+            return Activator.CreateInstance<TDerivedEntity>();
+        }
+
+        public override ObservableCollection<TEntity> Local
+        {
+            get { return _data; }
+        }
+
+        Type IQueryable.ElementType
+        {
+            get { return _query.ElementType; }
+        }
+
+        Expression IQueryable.Expression
+        {
+            get { return _query.Expression; }
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new TestDbAsyncQueryProvider<TEntity>(_query.Provider); }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator<TEntity> IEnumerable<TEntity>.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IDbAsyncEnumerator<TEntity> IDbAsyncEnumerable<TEntity>.GetAsyncEnumerator()
+        {
+            return new TestDbAsyncEnumerator<TEntity>(_data.GetEnumerator());
+        }
+    }
+
+    internal class TestDbAsyncQueryProvider<TEntity> : IDbAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        internal TestDbAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new TestDbAsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new TestDbAsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _inner.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+
+        public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute(expression));
+        }
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute<TResult>(expression));
+        }
+    }
+
+    internal class TestDbAsyncEnumerable<T> : EnumerableQuery<T>, IDbAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestDbAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestDbAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IDbAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            return new TestDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
+        {
+            return GetAsyncEnumerator();
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new TestDbAsyncQueryProvider<T>(this); }
+        }
+    }
+
+    internal class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestDbAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
+        }
+
+        public T Current
+        {
+            get { return _inner.Current; }
+        }
+
+        object IDbAsyncEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
