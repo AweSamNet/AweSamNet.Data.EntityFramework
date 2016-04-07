a5cee9f884f3abd8479b7ff55b7c40c8dacda1ce
Sam Lombardo <samlombardo1981@gmail.com>
2016-04-07 02:07:29 -0400

    - add extension method to add a range of items to an entity's navigation property

---
+++ ExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AweSamNet.Data.EntityFramework
+{
+    public static class ExtensionMethods
+    {
+        public static void TryAddRangeToNavigationProperty<T, TProperty>(this IDbContext db, T entity,
+            TProperty[] collection, Func<TProperty, object> key, Func<T, ICollection<TProperty>> navigationProperty)
+            where T : class
+            where TProperty : class, new()
+        {
+            db.Set<T>().Attach(entity);
+
+            //attach any existing entities
+            foreach (var item in collection.Where(item => key(item) != key(new TProperty())))
+            {
+                db.Set<TProperty>().Attach(item);
+                if (navigationProperty(entity).All(t => key(t) != key(item)))
+                {
+                    navigationProperty(entity).Add(item);
+                }
+            }
+        }
+    }
+}
